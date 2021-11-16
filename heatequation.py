0462ee100c8e84c3feb39228005cb1eb4b8626f1
vetleoyeopheim <93449581+vetleoyeopheim@users.noreply.github.com>
2021-11-16 22:19:24 +0100

    Update heatequation.py

--- heatequation.py
+++ heatequation.py
@@ -5,16 +5,65 @@ Created on Wed Nov 10 18:58:02 2021
 @author: Vetle
 """
 
-
-import numpy as np
-
-
-
 class HeatEquation():
     
     def __init__(self):
         pass
 
+    def heat_eq_3d(self, temp_0, ranges, deltas, b_vals, alpha):
+        """
+        Parameters:
+            temp_0: Initial temperature distribution, must be 3D array
+            ranges: Range of values for t,x,y,z
+            deltas: deltas = [dt,dx,dy,dz]
+            b_vals: boundary values [x,y,z]
+            alpha: coefficient of diffusion
+        """
+        nt = int(ranges[0] / deltas[0])
+        nx = int(ranges[1] / deltas[1])
+        ny = int(ranges[2] / deltas[2])
+        nz = int(ranges[3] / deltas[3])
+        r1 = (alpha * deltas[0]) / (deltas[1] * deltas[1])
+        r2 = (alpha * deltas[0]) / (deltas[2] * deltas[2])
+        r3 = (alpha * deltas[0]) / (deltas[3] * deltas[3])
+        
+        temps = np.zeros((nt, nx, ny, nz))
+        temps[0] = temp_0
+        
+        t = 1
+
+        while t < nt - 1:
+            xyz_arr = np.zeros((nx,ny,nz))
+            x = 0
+            while x < nx - 1:
+                xy_arr = np.zeros((nx,ny))
+                y = 0
+                while y < ny - 1:
+                    z_row = np.zeros(nz)
+                    z = 0
+                    while z < nz - 1:
+                        if z == 0 or z == nz - 1:
+                            T = b_vals[2]
+                        elif y == 0 or y == ny - 1:
+                            T = b_vals[1]
+                        elif x == 0 or x == nx - 1:
+                            T = b_vals[0]
+                        else:
+                            T = temps[t - 1][x][y][z] \
+                            + r1 * (temps[t - 1][x - 1][y][z] + temps[t - 1][x + 1][y][z] - 2 * temps[t - 1][x][y][z]) \
+                            + r2 * (temps[t - 1][x][y - 1][z] + temps[t - 1][x][y + 1][z] - 2 * temps[t - 1][x][y][z]) \
+                            + r3 * (temps[t - 1][x][y][z - 1] + temps[t - 1][x][y][z + 1] - 2 * temps[t - 1][x][y][z])
+                        z_row[z] = T
+                        z += 1
+                    xy_arr[y] = z_row
+                    y += 1
+                xyz_arr[x] = xy_arr
+                x += 1
+            temps[t] = xyz_arr
+            t += 1
+
+        return temps
+    
 
     def heat_eq_2d(self, temp_0, ranges, deltas, b_vals, alpha):
         """
@@ -48,8 +97,9 @@ class HeatEquation():
                     elif x == 0 or x == nx:
                         T = b_vals[0]
                     else:
-                        T = temps[t - 1][x][y] + r1 * (temps[t - 1][x - 1][y] + temps[t - 1][x + 1][y] - 2 * temps[t - 1][x][y]) \
-                                            + r2 * (temps[t - 1][x][y - 1] + temps[t - 1][x][y + 1] - 2 * temps[t - 1][x][y])
+                        T = temps[t - 1][x][y] \
+                        + r1 * (temps[t - 1][x - 1][y] + temps[t - 1][x + 1][y] - 2 * temps[t - 1][x][y]) \
+                        + r2 * (temps[t - 1][x][y - 1] + temps[t - 1][x][y + 1] - 2 * temps[t - 1][x][y])
                     y_row[y] = T
                     y += 1
                 xy_arr[x] = y_row
@@ -94,5 +144,3 @@ class HeatEquation():
             temps[t] = row
             t += 1
         return temps
-
-
